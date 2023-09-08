59f34d3ee101239004a8bc217607bcd6d34ca9dd
majo06005 <majo06005@E-PW0234K2>
2023-09-08 13:39:02 +0200

    Add project files.

---
+++ Övning 2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket årtal är det");
+            string årtalText = Console.ReadLine();
+            int årtal = int.Parse(årtalText);
+            int årtalSenare = årtal + 20;
+            Console.WriteLine("Om 20 år är det år " +  årtalSenare);
+            Console.ReadKey();
+        }
+    }
+}
